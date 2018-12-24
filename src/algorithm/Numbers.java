1504d39ba6034e01770c5005ff556e51a480337a
animeshpartha <partha_uk@hotmail.com>
2018-12-24 13:53:05 -0500

    Animesh's Exam Solution

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -21,18 +21,19 @@ public class Numbers {
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
-		int [] num = new int[10];
+
+		int [] num = new int[10000];// it suppose to be 1M
 		storeRandomNumbers(num);
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		//ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
 		Sort algo = new Sort();
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
+		//  connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+		//  List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
+		//   printValue(numbers);
+
 		int n = num.length;
 		randomize (num, n);
 		//Insertion Sort
@@ -42,6 +43,19 @@ public class Numbers {
 
 		//By following above, Continue for rest of the Sorting Algorithm....
 
+		storeRandomNumbers(num);
+
+		randomize (num, n);
+		//Bubble sort
+		algo.bubbleSort(num);
+		long bubbleSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of "+ num.length + " numbers in bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
+
+		//randomize(num,n);
+		// merge sort
+		algo.mergeSort(num);
+		long mergeSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of "+ num.length + " numbers in merge Sort take: " + mergeSortExecutionTime + " milli sec");
 
 
 		//Come to conclusion about which Sorting Algo is better in given data set.
