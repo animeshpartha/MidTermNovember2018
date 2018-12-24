609596f38fc94b29c8ad362ec416d9749fe5212b
animeshpartha <partha_uk@hotmail.com>
2018-12-23 22:13:10 -0500

    Animesh's Exam Solution

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -3,6 +3,7 @@ package datastructure;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 public class CollectionView {
 
@@ -17,6 +18,11 @@ public static void main(String[] args) {
         map.put(4, "Boston");
         map.put(5, "San Jose");
         map.put(6, "Seattle");
+
+        Set<Integer> keys = map.keySet();
+        for (Integer key:keys){
+                System.out.println(key + " "+"Represent: "+map.get(key));
+        }
 
 
 
--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,8 +1,14 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class DataReader {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * User API to read the below textFile and print to console.
 		 * Use BufferedReader class. 
@@ -20,8 +26,15 @@ public class DataReader {
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
 
-
-
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(textFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String text ;
+		while ((text = br.readLine())!=null){
+			System.out.println(text);
+		}
 	}
-
 }
